b48bb2bd25d148d0e840ee286c4149bc65ba602c
Linret07 <galasirinska@gmail.com>
2026-09-02 19:53:58 +0300

    hw2

--- src/hw/hw2/Car.java
+++ src/hw/hw2/Car.java
@@ -3,17 +3,30 @@ package hw.hw2;
 public class Car {
     private final String model;
     private final int year;
-    private final int power;
+    private final Engine engine;
 
     public Car(String model, int year, int power) {
         this.model = model;
         this.year = year;
-        this.power = power;
+        this.engine = new Engine(power);
     }
 
     @Override
     public String toString() {
         return "Car{model='" + model + '\'' +
-                ", year=" + year + ", power=" + power + '}';
+                ", year=" + year + ", engine=" + engine + '}';
+    }
+
+    private static class Engine {
+        private final int power;
+
+        private Engine(int power) {
+            this.power = power;
+        }
+
+        @Override
+        public String toString() {
+            return "Engine{power=" + power + '}';
+        }
     }
 }
