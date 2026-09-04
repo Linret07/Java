98cb9da805145e84896360aaa66058ae8db3c952
Linret07 <galasirinska@gmail.com>
2026-09-04 10:49:29 +0300

    lesson3_6

---
+++ src/lessons/lesson3/lesson3_6/Main.java
@@ -0,0 +1,18 @@
+package lessons.lesson3.lesson3_6;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(11);
+        integers.add(22);
+        integers.add(-22);
+        integers.add(0);
+        integers.add(100500);
+
+        integers.sort(Comparator.comparingInt(o -> o));
+        System.out.println(integers);
+    }
+}
