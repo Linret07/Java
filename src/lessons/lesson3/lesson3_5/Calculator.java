b7c956cfd34fc62acb9be22f922ba3fd1976854f
Linret07 <galasirinska@gmail.com>
2026-09-04 10:44:37 +0300

    lesson3_5

---
+++ src/lessons/lesson3/lesson3_5/Calculator.java
@@ -0,0 +1,8 @@
+package lessons.lesson3.lesson3_5;
+
+public interface Calculator {
+    int calculate(int a, int b);
+
+    default void qwe() {
+    }
+}
---
+++ src/lessons/lesson3/lesson3_5/Main.java
@@ -0,0 +1,7 @@
+package lessons.lesson3.lesson3_5;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = (a, b) -> 0;
+    }
+}
